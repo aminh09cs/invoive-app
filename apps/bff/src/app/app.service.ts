58007227f73324bdf936bca44b30b6e407ce3e3a
aminh09cs <18521098@gm.uit.edu.vn>
2026-01-18 23:17:38 +0700

    feat: add constants library with configuration files and initial PORT constant

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PORT } from '@common/constants/lib/common.constant';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    console.log('API is running', PORT);
     return { message: 'Hello API' };
   }
 }
